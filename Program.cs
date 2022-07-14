1602a1a5a4ffe4f836ac487ce1ea1e8bb7dee3b8
randerthe <randerlealt@gmail.com>
2022-07-15 00:39:34 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace SmallestIntervalMinIntervalMinDistance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = { -1, 2, 5, 10, 20, 25 };
+            int result = minDist(array);
+
+            Console.WriteLine(result);
+
+        }
+
+        static int minDist(int[] arr)
+        {
+            int MinDistance = arr.SelectMany(
+                (a, i) => arr.Skip(i + 1).Select((b) => Math.Abs(a - b))).Min();
+
+            return MinDistance;
+        }
+    }
+}
